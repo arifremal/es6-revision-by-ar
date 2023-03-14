431c8201dc21854e3100433a212e64e0ab7b82dc
arifremal <arifremal013@gmail.com>
2023-03-14 14:25:32 +0600

    added

---
+++ es-6.js
@@ -0,0 +1,10 @@
+const number =[90,5,66,7,90]
+const n = [...number]
+
+number.push(09)
+number.push(09)
+number.push(09)
+console.log(number);
+console.log(n);
+const add = [...number,100]
+console.log(add);
